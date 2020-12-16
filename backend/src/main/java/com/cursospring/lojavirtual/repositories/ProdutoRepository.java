3d7be18ba68dfc13334af91fab57e2367499a0f0
DouglasMonteles <silvadouglas328@gmail.com>
2020-12-16 20:58:35 -0300

    Produto e associacao muitos para muitos

---
+++ backend/src/main/java/com/cursospring/lojavirtual/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.cursospring.lojavirtual.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cursospring.lojavirtual.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -3,3 +3,12 @@
 INSERT INTO tb_categoria (nome) VALUES ('Informática');
 INSERT INTO tb_categoria (nome) VALUES ('Eletrônicos');
 INSERT INTO tb_categoria (nome) VALUES ('Limpeza');
+
+INSERT INTO tb_produto (nome, preco) VALUES ('Computador', 2000.00);
+INSERT INTO tb_produto (nome, preco) VALUES ('Máquina de Lavar', 600.00);
+INSERT INTO tb_produto (nome, preco) VALUES ('Rádio Portátil', 200.00);
+
+INSERT INTO tb_produto_categoria (produto_id, categoria_id) VALUES (1, 1);
+INSERT INTO tb_produto_categoria (produto_id, categoria_id) VALUES (1, 2);
+INSERT INTO tb_produto_categoria (produto_id, categoria_id) VALUES (2, 3);
+INSERT INTO tb_produto_categoria (produto_id, categoria_id) VALUES (3, 2);
