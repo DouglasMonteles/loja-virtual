3540e38796139c64a9e6548500ef632395b2b2f2
DouglasMonteles <silvadouglas328@gmail.com>
2020-12-16 20:17:44 -0300

    Inserindo dados de forma automatica na tabela de Categorias

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+/*ARQUIVO PARA INSERSÃO DE DADOS DE FORMA AUTOMÁTICA E TEMPORÁRIA (H2)*/
+
+INSERT INTO tb_categoria (nome) VALUES ('Informática');
+INSERT INTO tb_categoria (nome) VALUES ('Eletrônicos');
+INSERT INTO tb_categoria (nome) VALUES ('Limpeza');
