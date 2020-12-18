20ee632b9c9aa06f5ab8fa8ed844ebdded44e185
DouglasMonteles <silvadouglas328@gmail.com>
2020-12-18 19:34:41 -0300

    Personalizando o retorno das excecoes de validacao

--- backend/src/main/java/com/cursospring/lojavirtual/resources/exceptions/ResourceExceptionHandle.java
+++ backend/src/main/java/com/cursospring/lojavirtual/resources/exceptions/ResourceExceptionHandle.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -23,6 +25,23 @@ public class ResourceExceptionHandle {
 	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
 		StandardError error = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
+	
+	@ExceptionHandler(NumberFormatException.class)
+	public ResponseEntity<StandardError> numberFormat(NumberFormatException e, HttpServletRequest request) {
+		StandardError error = new StandardError(HttpStatus.BAD_REQUEST.value(), "Erro na formatação do parâmetro", System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		ValidationError errors = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Bad Request", System.currentTimeMillis());
+		
+		for (FieldError error : e.getBindingResult().getFieldErrors()) {
+			errors.setErrors(error.getField(), error.getDefaultMessage());
+		}
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
 	}
 	
 }
