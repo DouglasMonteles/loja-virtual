dceefc9990291ce7f393f9aef2dafef7698d5d1a
DouglasMonteles <silvadouglas328@gmail.com>
2020-12-25 17:19:13 -0300

    Criando o modulo de landing-page

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -2,7 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: 'landing-page', 
+    loadChildren: () => import('./views/landing-page/landing-page.module').then(m => m.LandingPageModule) 
+  },
+  {
+    path: '',
+    redirectTo: 'landing-page',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
