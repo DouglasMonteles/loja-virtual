19b8c6274bb2bb3715e99ad45133a8eeea75d007
DouglasMonteles <silvadouglas328@gmail.com>
2020-12-26 00:15:15 -0300

    Implementando a landingPage

--- web/src/app/views/landing-page/landing-page.module.ts
+++ web/src/app/views/landing-page/landing-page.module.ts
@@ -4,12 +4,18 @@ import { CommonModule } from '@angular/common';
 import { LandingPageRoutingModule } from './landing-page-routing.module';
 import { LandingPageComponent } from './landing-page.component';
 
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [LandingPageComponent],
   imports: [
     CommonModule,
-    LandingPageRoutingModule
+    LandingPageRoutingModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatIconModule,
   ]
 })
 export class LandingPageModule { }
