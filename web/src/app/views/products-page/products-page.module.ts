fb861f58e1d3cdcae69340352f9c31fed7ee4c71
DouglasMonteles <silvadouglas328@gmail.com>
2020-12-27 14:32:56 -0300

    Criando exibicao de produtos com card e grid css

--- web/src/app/views/products-page/products-page.module.ts
+++ web/src/app/views/products-page/products-page.module.ts
@@ -5,6 +5,10 @@ import { ProductsPageRoutingModule } from './products-page-routing.module';
 import { ProductsPageComponent } from './products-page.component';
 import { MenuModule } from 'src/app/components/menu/menu.module';
 import { RodapeModule } from 'src/app/components/rodape/rodape.module';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -15,6 +19,10 @@ import { RodapeModule } from 'src/app/components/rodape/rodape.module';
     ProductsPageRoutingModule,
     MenuModule,
     RodapeModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
   ]
 })
 export class ProductsPageModule { }
